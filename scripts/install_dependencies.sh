9f2972198120c8f83476b08b39eb8888b40bae07
wallowitch2 <129733734+wallowitch2@users.noreply.github.com>
2023-04-11 18:12:18 +0900

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www/my-nodejs-app
-rm -fr /var/www/my-nodejs-app/
+rm -fr /var/www/my-nodejs-app/*
 npm install express body-parser express-session session-file-store method-override
